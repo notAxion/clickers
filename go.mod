45f9ddef4459c6f8086341315c9a9494efc2e9f3
notAxion <mr.bhakat456@gmail.com>
2023-05-19 00:14:09 +0530

    first try to create a wav file from sin and cos wave

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module sound
 
 go 1.20
 
+require github.com/go-audio/wav v1.1.0
+
 require (
 	github.com/go-audio/audio v1.0.0 // indirect
 	github.com/go-audio/riff v1.0.0 // indirect
-	github.com/go-audio/wav v1.1.0 // indirect
 )
-
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"encoding/binary"
+	"flag"
 	"fmt"
 	"log"
 	"math"
 	"os"
+
+	"github.com/go-audio/audio"
+	"github.com/go-audio/wav"
 )
 
 const (
@@ -15,31 +18,92 @@ const (
 )
 
 func main() {
+	outputFile := ""
+	flag.StringVar(&outputFile, "output", "out.wav", "[[o]utput] file for the program")
+	flag.StringVar(&outputFile, "o", "out.wav", "[[o]utput] file for the program")
+	flag.Parse()
 	nsamps := Duration * SampleRate
 	angle := (math.Pi * 2) / float64(nsamps)
-	file, err := os.Create("out.bin")
+	egFile, err := os.Open("fixtures_32bit.wav")
 	if err != nil {
-		log.Println(err)
+		log.Fatalln(err)
+	}
+	egDecoder := wav.NewDecoder(egFile)
+	egBuf, err := egDecoder.FullPCMBuffer()
+	if err != nil {
+		log.Fatalln(err)
 	}
 
+	f, err := os.Open(outputFile)
+	defer f.Close()
+	if err == nil {
+		log.Fatalln(outputFile + " already exists use something else")
+	}
+	outFile, err := os.Create(outputFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	outEncoder := wav.NewEncoder(
+		outFile,
+		SampleRate, // egBuf.Format.SampleRate,
+		int(egDecoder.BitDepth),
+		1, // egBuf.Format.NumChannels,
+		int(egDecoder.WavAudioFormat),
+	)
+
+	outData := make([]int, nsamps)
 	for i := 0; i < nsamps; i++ {
 		sample := math.Sin(angle * Frequency * float64(i))
-		var buf [8]byte
+		outData[i] = int(sample * math.MaxInt32)
+
+		if outData[i] < math.MinInt32 || outData[i] > math.MaxInt32 {
+			fmt.Println(sample, outData[i])
+		}
+		// var buf [8]byte
 
 		// i checked mine is LittleEndian cpu
 		// might have to change to BigEndian for other CPU
-		binary.LittleEndian.PutUint32(buf[:], math.Float32bits(float32(sample)))
-		nBytes, err := file.Write(buf[:])
-		if err != nil {
-			log.Println("error while writing to the file")
-			log.Println(err)
-		} else {
-			// don't really know what this \r did
-			// but it seems like it is printing on the same line somehow
-			fmt.Printf("\rWrote: %d bytes to the file", nBytes)
-		}
-
+		// binary.LittleEndian.PutUint32(buf[:], math.Float32bits(float32(sample)))
+		// fmt.Println(int(sample * 10e10))
+		// if i > 100 {
+		// 	break
+		// }
+		// nBytes, err := outEncoder.Write(buf[:])
+		// if err != nil {
+		// 	log.Println("error while writing to the file")
+		// 	log.Println(err)
+		// } else {
+		// 	// don't really know what this \r did
+		// 	// but it seems like it is printing on the same line somehow
+		// 	fmt.Printf("\rWrote: %d bytes to the file", nBytes)
+		// }
 	}
+
+	outBuffer := audio.IntBuffer{
+		Format:         egBuf.Format,
+		Data:           outData,
+		SourceBitDepth: egBuf.SourceBitDepth,
+	}
+
+	err = outEncoder.Write(&outBuffer)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = outEncoder.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// reopen to confirm things worked well
+	// out, err := os.Open("out.wav")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// d2 := wav.NewDecoder(out)
+	// d2.ReadInfo()
+	// fmt.Println("New file ->", d2)
+	// out.Close()
 }
 
 // i think this is the sample rate
